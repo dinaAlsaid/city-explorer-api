76d58fc28ed0490448687af655750440365bb8ce
dinaAlsaid <dinaAlsaid@github.com>
2020-08-30 16:38:20 +0300

    cors

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const server = express();
 require('dotenv').config();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 2000;
+server.use(cors());
 
 //main page
 server.get('/', (req, res) => {
@@ -31,7 +33,25 @@ function Location(city,data){
     this.longotude = data[0].lon;
 }
 
+// server.get('/weather', (req , res) => {
+//     const locationData = require('./data/weather.json');
+//     let foreCast =new Forecast(weatherData);
+    // console.log(city);
+    // locationData.forEach(item => {
+    //     location.push( new Location(city,item) );
+    // })
+    // res.send(location);
 
+    // res.send("let's see what happens"); we can only have one response (will cause an error)
+// });
+// function Location(weather){
+//     this.forecast= ;
+//     this.time = weather.data.display_name;
+// }
+// {
+//     "forecast": "Partly cloudy until afternoon.",
+//     "time": "Mon Jan 01 2001"
+//   },
 server.listen(PORT, () => {
     console.log("Serevr is up");
 });
