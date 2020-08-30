ac3edf6be5e8e931b50f6f576a4c3f3c2bfb746f
dinaAlsaid <dinaAlsaid@github.com>
2020-08-30 21:31:44 +0300

    forecast data

--- server.js
+++ server.js
@@ -14,44 +14,68 @@ server.get('/', (req, res) => {
 });
 
 //location route 
-server.get('/location', (req , res) => {
+server.get('/location', (req, res) => {
     const locationData = require('./data/location.json');
     let city = req.query.city;
-    let location=new Location(city,locationData);
-    // console.log(city);
-    // locationData.forEach(item => {
-    //     location.push( new Location(city,item) );
-    // })
+    let location = new Location(city, locationData);
     res.send(location);
 
     // res.send("let's see what happens"); we can only have one response (will cause an error)
 });
-function Location(city,data){
+function Location(city, data) {
     this.search_query = city;
     this.formatted_query = data[0].display_name;
     this.latitude = data[0].lat;
     this.longitude = data[0].lon;
 }
 
-// server.get('/weather', (req , res) => {
-//     const locationData = require('./data/weather.json');
-//     let foreCast =new Forecast(weatherData);
-    // console.log(city);
-    // locationData.forEach(item => {
-    //     location.push( new Location(city,item) );
-    // })
-    // res.send(location);
+//weather route
+server.get('/weather', (req, res) => {
+    const weatherData = require('./data/weather.json');
+    let foreCast = [];
+    weatherData.data.forEach(item => {
+        foreCast.push(new Forecast(item));
+    })
+    res.send(foreCast);
 
     // res.send("let's see what happens"); we can only have one response (will cause an error)
-// });
-// function Location(weather){
-//     this.forecast= ;
-//     this.time = weather.data.display_name;
-// }
-// {
-//     "forecast": "Partly cloudy until afternoon.",
-//     "time": "Mon Jan 01 2001"
-//   },
+});
+
+function Forecast(weatherDay) {
+    this.forecast= weatherDay.weather.description ;
+    this.time = `${this.weekDay(weatherDay)}, ${weatherDay.datetime}`;
+};
+
+Forecast.prototype.weekDay = function(weatherDay) {
+    let date = new Date (weatherDay.datetime);
+    switch (date.getDay()) {
+        case 0:
+            return 'Sun'
+            break;
+        case 1:
+            return 'Mon'
+            break;
+        case 2:
+            return 'Tue'
+            break;
+        case 3:
+            return 'Wed'
+            break;
+        case 4:
+            return 'Thu'
+            break;
+        case 5:
+            return 'Fri'
+            break;
+        case 6:
+            return 'Sat'
+            break;
+                                                            
+    };
+
+};
+
+
 server.listen(PORT, () => {
     console.log("Serevr is up");
 });
