78e3a2f00fcb06ebee7016bf8fc31f0ce9a3ce2c
dinaAlsaid <dinaAlsaid@github.com>
2020-09-01 23:32:50 +0300

    gets data from data base but not from api

---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS t1;
+
+CREATE TABLE IF NOT EXISTS t1 (
+    search_query VARCHAR(255),
+    formatted_query VARCHAR(255),
+    latitude VARCHAR(255),
+    longitude VARCHAR(255)
+);
